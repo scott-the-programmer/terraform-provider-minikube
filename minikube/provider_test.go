bcae635b3319d4f1f00e780ce54cb1f85a3f9482
Scott Murray <scott@murray.kiwi>
2022-09-13 23:19:49 +1200

    added test for provider initialisation

--- minikube/provider_test.go
+++ minikube/provider_test.go
@@ -1,6 +1,7 @@
 package minikube
 
 import (
+	"context"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -14,4 +15,23 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_impl(t *testing.T) {
 	var _ *schema.Provider = Provider()
+}
+
+func TestProvider_bootstrap(t *testing.T) {
+	provider := Provider()
+
+	sch := map[string]*schema.Schema{
+		"kubernetes_version": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The Kubernetes version that the minikube VM will use. Defaults to 'stable'.",
+			Default:     "v99.99.99",
+		},
+	}
+
+	rawC := make(map[string]interface{})
+
+	data := schema.TestResourceDataRaw(t, sch, rawC)
+
+	provider.ConfigureContextFunc(context.TODO(), data)
 }

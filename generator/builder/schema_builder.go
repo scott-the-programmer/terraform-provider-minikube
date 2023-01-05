397e1b0222a5428b2a18849af59a7f1201de2406
Scott Murray <scott@murray.kiwi>
2023-01-06 09:45:47 +1300

    introduced schema generator tool for generating schema_cluster.go from 'minikube start --help'

---
+++ generator/builder/schema_builder.go
@@ -0,0 +1,253 @@
+package builder
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type MinikubeBinary interface {
+	GetVersion(ctx context.Context) (string, error)
+	GetStartHelpText(ctx context.Context) (string, error)
+}
+
+type MinikubeHostBinary struct {
+}
+
+func (m *MinikubeHostBinary) GetVersion(ctx context.Context) (string, error) {
+	return run(ctx, "version")
+}
+
+func (m *MinikubeHostBinary) GetStartHelpText(ctx context.Context) (string, error) {
+	return run(ctx, "start", "--help")
+}
+
+var computedFields []string = []string{
+	"addons",
+	"apiserver_ips",
+	"apiserver_names",
+	"hyperkit_vsock_ports",
+	"insecure_registry",
+	"iso_url",
+	"nfs_share",
+	"ports",
+	"registry_mirror",
+	"client_key",
+	"client_certificate",
+	"cluster_ca_certificate",
+	"host",
+}
+
+func run(ctx context.Context, args ...string) (string, error) {
+	cmd := exec.CommandContext(ctx, "minikube", args...)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+type SchemaEntry struct {
+	Parameter   string
+	Default     string
+	Description string
+	Type        SchemaType
+	ArrayType   SchemaType
+}
+
+type SchemaBuilder struct {
+	targetFile string
+	minikube   MinikubeBinary
+}
+
+type SchemaType string
+
+const (
+	String SchemaType = "String"
+	Int    SchemaType = "Int"
+	Bool   SchemaType = "Bool"
+	Array  SchemaType = "List"
+)
+
+func NewSchemaBuilder(targetFile string, minikube MinikubeBinary) *SchemaBuilder {
+	return &SchemaBuilder{targetFile: targetFile, minikube: minikube}
+}
+
+func (s *SchemaBuilder) Build() error {
+	minikubeVersion, err := s.minikube.GetVersion(context.Background())
+	if err != nil {
+		return errors.New("could not run minikube binary. please ensure that you have minikube installed and available")
+	}
+
+	log.Printf("building schema for minikube version: %s", minikubeVersion)
+
+	help, err := s.minikube.GetStartHelpText(context.Background())
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(help))
+
+	entries := make([]SchemaEntry, 0)
+	var currentParameter string
+	var currentType SchemaType
+	var currentDefault string
+	var currentDescription string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+
+		switch {
+		case strings.HasPrefix(line, "--"):
+			currentDescription = ""
+			seg := strings.Split(line, "=")
+			currentParameter = strings.TrimPrefix(seg[0], "--")
+			currentParameter = strings.Replace(currentParameter, "-", "_", -1)
+			fmt.Printf("processing %s \n", currentParameter)
+			currentDefault = strings.TrimSuffix(seg[1], ":")
+			currentType = getSchemaType(currentDefault)
+			if currentType == String {
+				currentDefault = strings.Trim(currentDefault, "'")
+			}
+
+		default:
+			if line != "" {
+				currentDescription += line
+				break
+			}
+
+			if currentParameter == "" {
+				break
+			}
+
+			currentDescription = strings.ReplaceAll(currentDescription, "\"", "\\\"")
+
+			switch currentType {
+			case String:
+				entries = append(entries, SchemaEntry{
+					Parameter:   currentParameter,
+					Default:     fmt.Sprintf("\"%s\"", currentDefault),
+					Type:        currentType,
+					Description: currentDescription,
+				})
+			case Bool:
+				entries = append(entries, SchemaEntry{
+					Parameter:   currentParameter,
+					Default:     currentDefault,
+					Type:        currentType,
+					Description: currentDescription,
+				})
+			case Int:
+				val, err := strconv.Atoi(currentDefault)
+				if err != nil {
+					// is it a timestamp?
+					time, err := time.ParseDuration(currentDefault)
+					if err != nil {
+						return err
+					}
+					val = int(time.Minutes())
+					currentDescription = fmt.Sprintf("%s (Configured in minutes)", currentDescription)
+				}
+				entries = append(entries, SchemaEntry{
+					Parameter:   currentParameter,
+					Default:     strconv.Itoa(val),
+					Type:        currentType,
+					Description: currentDescription,
+				})
+			case Array:
+				entries = append(entries, SchemaEntry{
+					Parameter:   currentParameter,
+					Type:        Array,
+					ArrayType:   String,
+					Description: currentDescription,
+				})
+			}
+
+			currentParameter = ""
+		}
+	}
+
+	header := `//go:generate go run ../generator/main.go $GOFILE
+// THIS FILE IS GENERATED - DO NOT MODIFY DIRECTLY
+package minikube
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+var (
+	clusterSchema = map[string]*schema.Schema{
+`
+
+	body := ""
+	for _, entry := range entries {
+		extraParams := ""
+		if !contains(computedFields, entry.Parameter) {
+			extraParams = `
+			Optional:    true,
+			ForceNew:    true,	
+			`
+		}
+		if entry.Type == Array {
+			extraParams = extraParams + fmt.Sprintf(`
+			Elem: &schema.Schema{
+				Type:	%s,
+			},
+			`, "schema.Type"+entry.ArrayType)
+		} else {
+			extraParams = extraParams + fmt.Sprintf(`
+			Default: %s,
+				`, entry.Default)
+		}
+
+		body = body + fmt.Sprintf(`
+		"%s": {
+			Type:       %s,
+			Description: "%s",
+			%s
+		},
+	`, entry.Parameter, "schema.Type"+entry.Type, entry.Description, extraParams)
+	}
+
+	footer := `
+	}
+)
+
+func GetClusterSchema() map[string]*schema.Schema {
+	return clusterSchema
+}
+	`
+
+	schema := header + body + footer
+
+	err = os.WriteFile(s.targetFile, []byte(schema), 0644)
+
+	return err
+}
+
+func getSchemaType(s string) SchemaType {
+	if strings.Count(s, "'") == 2 || s == "" {
+		return String
+	} else if s == "true" || s == "false" {
+		return Bool
+	} else if strings.HasPrefix(s, "[") && strings.HasSuffix(s, "]") {
+		return Array
+	}
+	return Int
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
---
+++ generator/go.mod
@@ -0,0 +1,3 @@
+module generator/m/v2
+
+go 1.19

7723c05bd8b9670b60a1d394a0aca9e481980ab1
Scott Murray <scott@murray.kiwi>
2024-01-28 12:36:22 +1300

    minor schema override for extra_config to ensure type isn't overridden on `make schema` (#128)

--- minikube/schema_cluster.go
+++ minikube/schema_cluster.go
@@ -365,7 +365,7 @@ var (
 		},
 	
 		"extra_config": {
-			Type:					schema.TypeList,
+			Type:					schema.TypeSet,
 			Description:	"A set of key=value pairs that describe configuration that may be passed to different components. 		The key should be '.' separated, and the first part before the dot is the component to apply the configuration to. 		Valid components are: kubelet, kubeadm, apiserver, controller-manager, etcd, proxy, scheduler 		Valid kubeadm parameters: ignore-preflight-errors, dry-run, kubeconfig, kubeconfig-dir, node-name, cri-socket, experimental-upload-certs, certificate-key, rootfs, skip-phases, pod-network-cidr",
 			
 			Optional:			true,
@@ -374,6 +374,7 @@ var (
 			Elem: &schema.Schema{
 				Type:	schema.TypeString,
 			},
+			
 		},
 	
 		"extra_disks": {

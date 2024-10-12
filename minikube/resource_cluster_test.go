4aec1043d4dbdf07636140b16f57fd8a6e0e0218
scott-the-programmer <scott@murray.kiwi>
2024-10-13 02:00:24 +1300

    add wait acceptance test

--- minikube/resource_cluster_test.go
+++ minikube/resource_cluster_test.go
@@ -237,6 +237,21 @@ func TestClusterCreation_HAControlPlane(t *testing.T) {
 	})
 }
 
+func TestClusterCreation_Wait(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers:    map[string]*schema.Provider{"minikube": Provider()},
+		CheckDestroy: verifyDelete,
+		Steps: []resource.TestStep{
+			{
+				Config: testAcceptanceClusterConfig_Wait("docker", "TestClusterCreationDocker"),
+				Check: resource.ComposeTestCheckFunc(
+					testPropertyExists("minikube_cluster.new", "TestClusterCreationDocker"),
+				),
+			},
+		},
+	})
+}
+
 func TestClusterCreation_Hyperkit(t *testing.T) {
 	if runtime.GOOS != "darwin" {
 		t.Skip("Hyperkit is only supported on macOS")
@@ -714,6 +729,21 @@ func testAcceptanceClusterConfig_HAControlPlane(driver string, clusterName strin
 		cpus = 2
 		memory = "6000GiB"
 		ha = true
+	}
+	`, driver, clusterName)
+}
+
+func testAcceptanceClusterConfig_Wait(driver string, clusterName string) string {
+	return fmt.Sprintf(`
+	resource "minikube_cluster" "new" {
+		driver = "%s"
+		cluster_name = "%s"
+		cpus = 2
+		memory = "6000GiB"
+
+		wait = [
+			"apps_running"
+		]
 	}
 	`, driver, clusterName)
 }
